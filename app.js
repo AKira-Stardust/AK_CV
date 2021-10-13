77bae0f1cf8d6bb45da75dcea7da6abde6d07455
Archana Kumar <archanakumar1808@gmail.com>
2021-10-13 18:33:13 +0530

    AK's CV - v1

---
+++ app.js
@@ -0,0 +1,21 @@
+//Require
+const express = require("express");
+
+//App Declaration
+const app = express();
+
+//Setting View Engine
+app.set("view engine", "ejs");
+
+//Static and Middleware
+app.use(express.static("public"));
+
+//GET REQUEST - BASE
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+//Setup Server at 3000
+app.listen(3000, () => {
+    console.log("I'm Listening..");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        headerBg: "#00A19D",
+        headerTxt: "#FFF8E5",
+        primaryBg: "#FEFBF3",
+        primaryTxt: "#9D9D9D",
+        cardBg:"#D4ECDD",
+        cardBg1: "#E05D5D",
+        cardBg2: "#B8DFD8",
+        cardBg3:"#E8F6EF",
+        cardBg4:"#F0E5CF",
+        cardBg5:"#F7F6F2",
+        cardTxt:"white"
+      },
+      fontFamily:{
+        body:["Nunito"],
+        fancy:["Kalam"]
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
