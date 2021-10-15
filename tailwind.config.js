52219a226f8e7e592badc570328fb068e57f51aa
Archana Kumar <archanakumar1808@gmail.com>
2021-10-15 18:27:49 +0530

    version3 responsive updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,25 @@ module.exports = {
         cardBg3:"#E8F6EF",
         cardBg4:"#F0E5CF",
         cardBg5:"#F7F6F2",
-        cardTxt:"white"
+        cardTxt:"white",
+        hoverTxt1:"#DBD0C0",
+        hoverTxt2:"#D9CAB3",
+        hoverTxt3:"#F5C6A5",
+        hoverTxt4:"#368B85",
+        hoverTxt5:"#01937C",
+        hoverTxt6:"#2F5D62"
       },
-      fontFamily:{
+      fontFamily: {
         body:["Nunito"],
         fancy:["Kalam"]
       }
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      fontSize: ['hover', 'focus','responsive'],
+      fontWeight: ['hover', 'focus','responsive']
+    },
   },
   plugins: [],
 }
