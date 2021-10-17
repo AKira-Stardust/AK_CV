45c01f8d29f91f24ca28866a7ca3742104c1d576
Archana Kumar <archanakumar1808@gmail.com>
2021-10-17 17:48:09 +0530

    v4.1 minor bug fixes and nav fixed

--- public/index.js
+++ public/index.js
@@ -1,10 +1,16 @@
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#small_menu");
+const wrapper = document.querySelector("#main_wrapper");
 
 burger.addEventListener("click", () => {
     if(menu.classList.contains("hidden")){
         menu.classList.remove("hidden");
+        wrapper.classList.remove("pt-18");
+        wrapper.classList.add("pt-80");
+
     } else {
         menu.classList.add("hidden");
+        wrapper.classList.add("pt-18");
+        wrapper.classList.remove("pt-80");
     }
 });
