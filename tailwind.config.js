f2a4e9a981c92810c3826db86099eee3616c480b
Archana Kumar <archanakumar1808@gmail.com>
2021-10-21 18:41:45 +0530

    purple edition v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        headerBg: "#00A19D",
+        // headerBg: "#00A19D",
+        headerBg:"#A03C78",
         headerTxt: "#FFF8E5",
         primaryBg: "#FEFBF3",
         primaryTxt: "#9D9D9D",
@@ -20,11 +21,26 @@ module.exports = {
         hoverTxt3:"#F5C6A5",
         hoverTxt4:"#368B85",
         hoverTxt5:"#01937C",
-        hoverTxt6:"#2F5D62"
+        hoverTxt6:"#2F5D62",
+        navHover1:"#F7DBF0",
+        navHover2:"#F0D9E7",
+        navHoverRed:"#F8485E",
+        navHoverCream:"#EBD8B7",
+        navHoverBlue1:"#1597E5",
+        navHoverBlue2:"#113CFC",
+        navHoverBlue3:"#193498",
+        navHoverBlue4:"#69DADB",
+        navHoverBrown:"#3F0713",
+        portfolioBlue1:"#1597E5",
+        portfolioBlue2:"#8FC1D4",
+        portfolioBlue3:"#53B8BB",
       },
       fontFamily: {
         body:["Nunito"],
         fancy:["Kalam"]
+      },
+      backgroundImage: {
+        'tokyo-cityscape':"url('img/bg_image.jpeg')"
       }
     },
   },
